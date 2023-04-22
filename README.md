5bc336358a5a398725234f37faedfdb040a11de2
Pavel <>
2023-04-22 13:35:11 +0300

    add seutp

--- README.md
+++
@@ -1,3 +0,0 @@
-# TinkoffSolutions
-
-ыаыаыа
---
+++ TinkoffSolutionsDS/Example/TinkoffSolutionsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tfs
+//
+//  Created by Protasov Pavel Sergeevich on 22.04.2023.
+//
+
+import UIKit
+import TinkoffSolutionsDS
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let f = MyLibrary().text
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
